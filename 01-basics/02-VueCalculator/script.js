103284958174b3a71d3ac825e707affe1e310d58
Дикова Евгения <dikova.e@bookcentre.ru>
2021-08-02 23:22:43 +0300

    VueCalculator was added

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,28 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const app = createApp({
+  data() {
+    return {
+      firstValue: null,
+      secondValue: null,
+      operand: null,
+    };
+  },
+  computed: {
+    result() {
+      switch (this.operand) {
+        case '+':
+          return this.firstValue + this.secondValue;
+        case '-':
+          return this.firstValue - this.secondValue;
+        case '*':
+          return this.firstValue * this.secondValue;
+        case '/':
+          return this.firstValue / this.secondValue;
+        default:
+          return 0;
+      }
+    },
+  },
+});
+app.mount('#app');
